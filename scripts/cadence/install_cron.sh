a356d07eb6d01190202a86e2bcffb0e7d8aaa798
uprootiny <uprootiny@gmail.com>
2026-02-17 07:39:26 +0100

    chore(cadence): schedule nightly log wrangling in cron installer

--- scripts/cadence/install_cron.sh
+++ scripts/cadence/install_cron.sh
@@ -18,6 +18,8 @@ cat >> "${TMP}.clean" <<EOF
 5 1 * * * cd ${ROOT_DIR} && ./scripts/cadence/daily_snapshot.sh ${BASE_URL} >> ${LOG_DIR}/cron-daily-snapshot.log 2>&1
 # ManicAI cadence: weekly benchmark drift (Mon 02:10 UTC)
 10 2 * * 1 cd ${ROOT_DIR} && ./scripts/cadence/weekly_benchmark_drift.py >> ${LOG_DIR}/cron-weekly-drift.log 2>&1
+# ManicAI cadence: nightly log wrangling
+25 2 * * * cd ${ROOT_DIR} && KEEP_DAYS=14 MAX_MB=256 ./scripts/cadence/wrangle_logs.sh >> ${LOG_DIR}/cron-log-wrangle.log 2>&1
 EOF
 
 crontab "${TMP}.clean"
